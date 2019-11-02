1e54f4f6d0868d750e858638f38e48057aa18211
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-02 09:48:32 +0100

    Add killAnySteamProcess() function

--- VAC-Bypass-Loader/main.c
+++ VAC-Bypass-Loader/main.c
@@ -99,6 +99,30 @@ VOID waitOnModule(DWORD processId, PCWSTR moduleName)
     }
 }
 
+VOID killAnySteamProcess()
+{
+    HANDLE processSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    PROCESSENTRY32W processEntry;
+    processEntry.dwSize = sizeof(processEntry);
+
+    if (Process32FirstW(processSnapshot, &processEntry)) {
+        PCWSTR steamProcesses[] = { L"Steam.exe", L"SteamService.exe", L"steamwebhelper.exe" };
+        do {
+            for (INT i = 0; i < sizeof(steamProcesses) / sizeof(steamProcesses[0]); i++) {
+                if (!lstrcmpW(processEntry.szExeFile, steamProcesses[i])) {
+                    HANDLE processHandle = OpenProcess(PROCESS_TERMINATE, FALSE, processEntry.th32ProcessID);
+                    if (processHandle) {
+                        TerminateProcess(processHandle, 0);
+                        CloseHandle(processHandle);
+                    }
+                }
+            }
+        } while (Process32NextW(processSnapshot, &processEntry));
+    }
+    CloseHandle(processSnapshot);
+}
+
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nShowCmd)
 {
     HKEY key = NULL;
